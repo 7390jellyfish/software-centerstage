43ec4d9b8a72f9a046a9af2116ffee3374a06ab1
mxtmx <maxwell.tham@gmail.com>
2023-12-24 14:06:03 -0800

    update auton

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonTest.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonTest.java
@@ -1,13 +1,11 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class AutonTest {
-
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -15,9 +13,8 @@ public class AutonTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, Math.toRadians(180)))
-                                .strafeLeft(40)
-                                .back(37)
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(10)
                                 .build()
                 );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CloseBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CloseBlue.java
@@ -34,20 +34,44 @@ public class CloseBlue extends LinearOpMode {
         leftLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        leftLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(0, 0, 0);
+        Pose2d startPose = new Pose2d(0, 0, 270);
 
         drive.setPoseEstimate(startPose);
 
         TrajectorySequence spikeMark = drive.trajectorySequenceBuilder(startPose)
                 .forward(10)
+                .addDisplacementMarker(() -> {
+                    intake.setPower(-1);
+                    sleep(1000);
+                })
                 .build();
         TrajectorySequence backdrop = drive.trajectorySequenceBuilder(spikeMark.end())
                 .forward(10)
+                .addDisplacementMarker(() -> {
+                    upDeposit();
+                })
                 .build();
         TrajectorySequence cycle = drive.trajectorySequenceBuilder(backdrop.end())
                 .forward(10)
+                .addDisplacementMarker(() -> {
+                    intake.setPower(1);
+                    transit.setPower(1);
+                    sleep(1000);
+                })
+                .forward(10)
+                .addDisplacementMarker(() -> {
+                    upDeposit();
+                    downDeposit();
+                    upDeposit();
+                })
                 .build();
 
         waitForStart();
@@ -56,6 +80,37 @@ public class CloseBlue extends LinearOpMode {
             drive.followTrajectorySequence(spikeMark);
             drive.followTrajectorySequence(backdrop);
             drive.followTrajectorySequence(cycle);
+            upDeposit();
+            downDeposit();
         }
+    }
+    void upDeposit() {
+        claw.setPosition(0);
+        sleep(1000);
+        leftLift.setTargetPosition(1500);
+        rightLift.setTargetPosition(1500);
+        leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftLift.setPower(1);
+        rightLift.setPower(1);
+        while (opModeIsActive() && (leftLift.isBusy() && rightLift.isBusy())) { }
+        leftLift.setPower(0);
+        rightLift.setPower(0);
+        leftLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        claw.setPosition(0.9);
+    }
+    void downDeposit() {
+        leftLift.setTargetPosition(0);
+        rightLift.setTargetPosition(0);
+        leftLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftLift.setPower(-1);
+        rightLift.setPower(-1);
+        while (opModeIsActive() && (leftLift.isBusy() && rightLift.isBusy())) { }
+        leftLift.setPower(0);
+        rightLift.setPower(0);
+        leftLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
